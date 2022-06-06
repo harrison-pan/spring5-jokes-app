ae65d1f323ddfc2699e4534d834ad25187f4c89d
Harrison Pan <hanshen36@gmail.com>
2022-06-06 22:43:36 +1200

    Add Spring service layer - JokeService

---
+++ src/main/java/com/harrison/spring5jokesapp/services/JokeService.java
@@ -0,0 +1,9 @@
+package com.harrison.spring5jokesapp.services;
+
+/**
+ * @author Harrison Pan
+ */
+public interface JokeService {
+
+  String getJoke();
+}
---
+++ src/main/java/com/harrison/spring5jokesapp/services/JokeServiceImpl.java
@@ -0,0 +1,22 @@
+package com.harrison.spring5jokesapp.services;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author Harrison Pan
+ */
+@Service
+public class JokeServiceImpl implements JokeService {
+
+  private final ChuckNorrisQuotes chuckNorrisQuotes;
+
+  public JokeServiceImpl() {
+    this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+  }
+
+  @Override
+  public String getJoke() {
+    return chuckNorrisQuotes.getRandomQuote();
+  }
+}
